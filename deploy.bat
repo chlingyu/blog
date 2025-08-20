a1026777b84cbfbbbc769c7b1423fecd9eee01d0
chlingyu <854450468@qq.com>
2025-08-20 23:09:29 +0800

    11

--- deploy.bat
+++ deploy.bat
@@ -1,44 +1,45 @@
 @echo off
+chcp 65001 >nul
 echo =======================================
-echo   部署 Astro 博客到 CentOS 服务器
+echo   Deploy Astro Blog to CentOS Server
 echo =======================================
 
-REM 服务器信息
+REM Server Configuration
 set SERVER=159.75.167.181
 set USER=root
 set REMOTE_PATH=/var/www/blog
 
 echo.
-echo [1/4] 构建项目...
+echo [1/4] Building project...
 call npm run build
 if errorlevel 1 (
-    echo 构建失败！请检查错误信息
+    echo Build failed! Please check error messages
     pause
     exit /b 1
 )
 
 echo.
-echo [2/4] 创建部署包...
+echo [2/4] Creating deployment package...
 cd dist
 tar -czf ../deploy.tar.gz *
 cd ..
 
 echo.
-echo [3/4] 上传到服务器...
-echo 需要输入服务器密码：
+echo [3/4] Uploading to server...
+echo Please enter server password:
 scp deploy.tar.gz %USER%@%SERVER%:/tmp/
 
 echo.
-echo [4/4] 在服务器上解压部署...
-ssh %USER%@%SERVER% "rm -rf %REMOTE_PATH%/* && tar -xzf /tmp/deploy.tar.gz -C %REMOTE_PATH% && chown -R nginx:nginx %REMOTE_PATH% && rm /tmp/deploy.tar.gz"
+echo [4/4] Extracting on server...
+ssh %USER%@%SERVER% "mkdir -p %REMOTE_PATH% && rm -rf %REMOTE_PATH%/* && tar -xzf /tmp/deploy.tar.gz -C %REMOTE_PATH% && chown -R nginx:nginx %REMOTE_PATH% && rm /tmp/deploy.tar.gz"
 
 echo.
 echo =======================================
-echo   部署完成！
-echo   访问: http://%SERVER%/blog/
+echo   Deployment Complete!
+echo   Visit: http://%SERVER%/blog/
 echo =======================================
 
-REM 清理本地临时文件
+REM Clean up temporary files
 del deploy.tar.gz
 
 pause
